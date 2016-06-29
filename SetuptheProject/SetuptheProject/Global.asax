f98b02161d40a1753bbc393fd9df1cd32e5cfc12
leng <lzyfems@163.com>
2016-06-29 18:32:58 +0800

    git init

---
+++ SetuptheProject/SetuptheProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SetuptheProject.MvcApplication" Language="C#" %>
---
+++ SignalRChat/SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addNewMessageToPage(name, message);
+        }
+
+        //自定义Method，首字母大小其他小写，客户端全部小写
+        public void Sendtest(string name, string value)
+        {
+            Clients.All.addTest(name, value);
+        }
+    }
+}

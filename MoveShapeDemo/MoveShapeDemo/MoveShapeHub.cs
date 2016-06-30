74378d2562414f37fd4b40fc3bcb4479ee99d0ec
leng <lzyfems@163.com>
2016-06-30 09:37:18 +0800

    Add MoveShapeDemo

---
+++ MoveShapeDemo/MoveShapeDemo/MoveShapeHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+
+namespace MoveShapeDemo
+{
+    public class MoveShapeHub : Hub
+    {
+        public void UpdateModel(ShapeModel clientModel)
+        {
+            clientModel.LastUpdatedBy = Context.ConnectionId;
+            Clients.AllExcept(clientModel.LastUpdatedBy).UpdateShape(clientModel);
+        }
+    }
+
+    public class ShapeModel
+    {
+        [JsonProperty("left")]
+        public double Left { get; set; }
+
+        [JsonProperty("top")]
+        public double Top { get; set; }
+
+        [JsonIgnore]
+        public string LastUpdatedBy { get; set; }
+    }
+}
